cd6b825b02215ac6a3133285fd35192a9ebba93b
Vinícius Ribeiro <vinicna123@hotmail.com>
2016-11-16 22:09:38 -0200

    Executado.

---
+++ Denuncias/Denuncias/Denuncias/Model/Denuncia.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Denuncias.Model
+{
+    public class Denuncia
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Texto { get; set; }
+
+        public string Endereco { get; set; }
+    }
+}
